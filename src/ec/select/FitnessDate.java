7e2d0caad6bf0bd82b0bffb0be2733693f9950dd
kubasub <kubasub12@gmail.com>
2015-04-25 15:30:16 -0400

    abstracted the type of date (i.e. the match calculation)

---
+++ src/ec/select/FitnessDate.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.select;
+
+import ec.Individual;
+
+/**
+ * This class represents a "Date" that two individuals can go on where a match
+ * is based on similarity in fitness.
+ */
+public class FitnessDate implements Date {
+
+    @Override
+    public double match(Individual first, Individual second) {
+        return Math.abs(first.fitness.fitness() - second.fitness.fitness());
+    }
+    
+}
